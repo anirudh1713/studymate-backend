31eb1c34bb185c25c5dd187a756fbb77267eb180
anirudh1713 <anirudhatodaria11@gmail.com>
2021-03-21 20:50:51 +0530

    Fix linting issue

--- validations/department.validation.js
+++ validations/department.validation.js
@@ -5,30 +5,30 @@ const knex = require('../db/db');
 const { duplicate } = require('../utils/db');
 
 const createDepartment = Joi.object({
-    name: Joi.string().required(),
-    code: Joi.number().required().external(async value => {
-        await duplicate('departments', 'code', value);
-    }),
-    tutionFee: Joi.number().required(),
-    faculties: Joi.array().items(
-        Joi.object({
-            facultyId: Joi.number().required(),
-        }).required()
-    ).external(async value => {
-        if (value && value.length >= 1) {
-            for (const faculty of value) {
-                const fac = await knex('faculties').select('*').where({
-                    id: faculty.facultyId,
-                    department_id: null,
-                });
-                if (fac.length !== 1) {
-                    throw new ValidationError(`Faculty with id ${faculty.facultyId} either does not exist or is already asociated with another department.`);
-                }
-            }
+  name: Joi.string().required(),
+  code: Joi.number().required().external(async (value) => {
+    await duplicate('departments', 'code', value);
+  }),
+  tutionFee: Joi.number().required(),
+  faculties: Joi.array().items(
+    Joi.object({
+      facultyId: Joi.number().required(),
+    }).required(),
+  ).external(async (value) => {
+    if (value && value.length >= 1) {
+      for (const faculty of value) {
+        const fac = await knex('faculties').select('*').where({
+          id: faculty.facultyId,
+          department_id: null,
+        });
+        if (fac.length !== 1) {
+          throw new ValidationError(`Faculty with id ${faculty.facultyId} either does not exist or is already asociated with another department.`);
         }
-    }),
+      }
+    }
+  }),
 });
 
 module.exports = {
-    createDepartment,
-}
+  createDepartment,
+};
